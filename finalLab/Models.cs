cd07d8f5b5f951a312b71a4b8f49d4fef1a6bd63
mingwang168 <mingwang0904@gmail.com>
2019-11-29 14:52:31 -0800

    2

---
+++ finalLab/Models.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+
+namespace finalLab
+{
+    class Models
+    {
+        public class FoodCartContext : DbContext
+        {
+            public DbSet<FoodCart> FoodCarts { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                options.UseSqlServer("Data Source=MINGWIN10\\SQLEXPRESS; Database=myDB;Trusted_Connection=True;");
+            }
+        }
+
+        public class FoodCart
+        {
+            [Key]
+            public string key { get; set; }
+            public string status { get; set; }
+            public string description { get; set; }
+            public string geo_localarea { get; set; }
+            public float longitude { get; set; }
+            public float latitude { get; set; }
+
+            public string location { get; set; }
+            public string vendor_type { get; set; }
+            public string business_name { get; set; }
+        }
+
+    }
+}
--- finalLab/Program.cs
+++ finalLab/Program.cs
@@ -5,17 +5,56 @@ using System.Text;
 using Nancy.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+using static finalLab.Models;
 
 namespace finalLab
 {
     class Program
     {
+        static class SqlHelper
+        {
+            public static SqlDataReader ExecuteReader(String connString, String commandText)
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    return reader;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+
+
             const string BASE_URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=food-vendors";
             string jsonString = CallRestMethod(BASE_URL);
             // Console.WriteLine(jsonString);
             Rootobject rootObject = JsonConvert.DeserializeObject<Rootobject>(jsonString);
+
+            FoodCartContext db = new FoodCartContext();
+            foreach (Record cart in rootObject.records)
+            {
+                db.Add(new FoodCart
+                {
+                    key = cart.fields.key,
+                    status = cart.fields.status,
+                    description=cart.fields.description,
+                    geo_localarea=cart.fields.geo_localarea,
+                    longitude=cart.fields.geom.coordinates[0],
+                    latitude=cart.fields.geom.coordinates[1],
+                    location=cart.fields.location,
+                    vendor_type=cart.fields.vendor_type,
+                    business_name=cart.fields.business_name
+
+                });
+            }
+
+            db.SaveChanges();
         }
         static string CallRestMethod(string uri)
         {
