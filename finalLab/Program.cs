b7f265876ffb1294d4ab8748cda53889e981dc59
mingwang168 <mingwang0904@gmail.com>
2019-11-29 12:01:22 -0800

    Add project files.

---
+++ finalLab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using Nancy.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace finalLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string BASE_URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=food-vendors";
+            string jsonString = CallRestMethod(BASE_URL);
+            // Console.WriteLine(jsonString);
+            Rootobject rootObject = JsonConvert.DeserializeObject<Rootobject>(jsonString);
+        }
+        static string CallRestMethod(string uri)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string stringResponse = responseStream.ReadToEnd();
+                return stringResponse;
+                response.Close();
+                responseStream.Close();
+            }
+            catch (Exception e)
+            {
+                return $"{{'Error':'{e.Message}'}}";
+            }
+        }
+
+    }
+}
